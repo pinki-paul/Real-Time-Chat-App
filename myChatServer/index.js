c160260363db66e6ecdadccae75377f9415b06d6
Pinki Paul <pinkipaul371@gmail.com>
2023-08-22 18:10:16 +0530

    Changes

--- myChatServer/index.js
+++ myChatServer/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
-const port = process.env.port || 8000;
+const port =  8000;
 
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
